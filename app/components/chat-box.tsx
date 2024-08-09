9259587fa2a126637596b74918c96be7d1477b82
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2024-08-08 23:23:20 -0400

    chatbox UI Skelton

--- app/components/chat-box.tsx
+++ app/components/chat-box.tsx
@@ -1,14 +1,16 @@
 "use client";
-import { Box, Stack } from "@mui/material";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 export const ChatBox = () => {
-  const [messages, setMessages] = useState({
-    role: "assistant",
-    content: `Hi, I'm the Headstarter Support Agent, how can I assit you today?`,
-  });
+  const [messages, setMessages] = useState([
+    {
+      role: "assistant",
+      content: `Hi, I'm the Headstarter Support Agent, how can I assist you today?`,
+    },
+  ]);
 
-  const [message, setMessage] = useState(" ");
+  const [message, setMessage] = useState("");
 
   return (
     <Box
@@ -16,13 +18,13 @@ export const ChatBox = () => {
       height={"100vh"}
       className="flex flex-col justify-center items-center"
     >
-      test
       <Stack
         width={"600px"}
         height={"700px"}
         direction={"column"}
         spacing={3}
-        className="p-2 "
+        border={"solid"}
+        className="p-2"
       >
         <Stack
           direction={"column"}
@@ -30,10 +32,39 @@ export const ChatBox = () => {
           maxHeight={"100%"}
           className="flex-grow overflow-auto"
         >
-          test
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display={"flex"}
+              justifyContent={
+                message.role === "assistant" ? "flex-start" : "flex-end"
+              }
+            >
+              <Box
+                bgcolor={
+                  message.role === "assistant"
+                    ? "primary.main"
+                    : "secondary.main"
+                }
+                color={"white"}
+                borderRadius={16}
+                p={3}
+              >
+                {message.content}
+              </Box>
+            </Box>
+          ))}
+        </Stack>
+        <Stack direction={"row"} spacing={2}>
+          <TextField
+            label="message"
+            fullWidth
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button variant="contained">Send</Button>
         </Stack>
       </Stack>
-      test
     </Box>
   );
 };
